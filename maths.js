3cf4af12cb24f457369bab670b70d5ab4b8c886e
henri_SO <henri.bechert@softwareone.com>
2021-09-13 16:58:25 +0200

    more javascript interactivity; start of orbit graphing

--- maths.js
+++ maths.js
@@ -3,9 +3,9 @@ function div(a, b) {
     return a/b>>0;
 }
 
-function modulateCircle(deg) {
+function modulateCircle(rad) {
     // modulo for angles in degrees
-    return (deg % 360) + (deg < 0 ? 360 : 0);
+    return (rad % (2*Math.PI)) + (rad < 0 ? (2*Math.PI) : 0);
 }
 
 function getDay(y, m, D) {
@@ -29,36 +29,37 @@ function ecl(d) {
     return getRadians(23.4393 - 3.563E-7 * d);
 }
 
+const y_earth = 365.256;
+
 /*  Class for the astrological elements and for updating them */
 
 class astroElement {
     constructor(N, i, w, a, e, M, update) {
-        this._N0 = getRadians(N)                     // longitude of the ascending node
-        this._i0 = getRadians(i);                    // inclination to the ecliptic (plane of the Earth's orbit)
-        this._w0 = getRadians(w);                    // argument of perihelion
+        this._N0 = modulateCircle(getRadians(N));                     // longitude of the ascending node
+        this._i0 = modulateCircle(getRadians(i));                    // inclination to the ecliptic (plane of the Earth's orbit)
+        this._w0 = modulateCircle(getRadians(w));                    // argument of perihelion
         this._a0 = a;                                // semi-major axis, or mean distance from Sun (set in AE, ideally)
         this._e0 = e;                                // eccentricity (0=circle, 0-1=ellipse, 1=parabola)
-        this._M0 = getRadians(M);                    // mean anomaly (0 at perihelion; increases uniformly with time)
+        this._M0 = modulateCircle(getRadians(M));                    // mean anomaly (0 at perihelion; increases uniformly with time)
         this._update = update;
-        this.updateElement(0);
+        this.elementAt(0);
     }
-    updateElement(d) {
+    elementAt(d) {
         // update orbital elements
-        this._N = getRadians(this._N0 + this._update.N*d);
-        this._i = getRadians(this._i0 + this._update.i*d);
-        this._w = getRadians(this._w0 + this._update.w*d);
+        this._N = modulateCircle(getRadians(this._N0 + this._update.N*d));
+        this._i = modulateCircle(getRadians(this._i0 + this._update.i*d));
+        this._w = modulateCircle(getRadians(this._w0 + this._update.w*d));
         this._a = this._a0 + this._update.a*d;
         this._e = this._e0 + this._update.e*d;
-        this._M = getRadians(this._M0 + this._update.M*d);
+        this._M = modulateCircle(getRadians(this._M0 + this._update.M*d));
         // additional variables
-        this._w1 = this._N + this._w;         // longitude of perihelion
-        this._L = this._M + this._w1;         // mean longitude
+        this._w1 = modulateCircle(this._N + this._w);         // longitude of perihelion
+        this._L = modulateCircle(this._M + this._w1);         // mean longitude
         this._q = this._a*(1-Math.E);         // perihelion distance
         this._Q = this._a*(1+Math.E);         // aphelion distance
         this._P = Math.pow(this._a, 1.5);     // orbital period (years if a is in AU, astronomical units)
-        /* TODO:
-        this._T = Epoch_of_M - (M(deg)/360_deg) / P // time of perihelion */
-        this._E = this._M + this._e*Math.sin(this._M) * (1 + this._e*Math.cos(this._M)); // eccentric anomaly
+        this._T = d-this._M/(Math.PI*2)*y_earth*this._P;                                                    // time of perihelion
+        this._E = modulateCircle(this._M + this._e*Math.sin(this._M) * (1 + this._e*Math.cos(this._M)));  // eccentric anomaly
     }
 }
 
@@ -67,21 +68,21 @@ class sunElement extends astroElement {
         super(N, i, w, a, e, M, update);
         this._type = "sun";
     }
-    updateElement(d) {
-        super.updateElement(d);
+    elementAt(d) {
+        super.elementAt(d);
         this._x = Math.cos(this._E) - this._e;                      // x-coordinate on elliptic plane
         this._y = Math.sqrt(1-this._e*this._e) * Math.sin(this._E); // y-coordinate on elliptic plane
         this._r = Math.sqrt(this._y*this._y+this._x*this._x);       // distance from earth
-        this._v = Math.atan2(this._y, this._x);                     // true anomaly (angle between position and perihelion)
-        this._lon = this._v + this._w;                              // true longitude
+        this._v = modulateCircle(Math.atan2(this._y, this._x));     // true anomaly (angle between position and perihelion)
+        this._lon = modulateCircle(this._v + this._w);              // true longitude
         this._xs = this._r * Math.cos(this._lon);                   // ecliptic rectangular geocentric x-coordinate
         this._ys = this._r * Math.sin(this._lon);                   // ecliptic rectangular geocentric y-coordinate
         this._xe = this._xs;                                        // equitorial rectangular geocentric x-coordinate
         this._ye = this._ys * Math.cos(ecl(d));                     // equitorial rectangular geocentric y-coordinate
         this._ze = this._ys * Math.sin(ecl(d));                     // equitorial rectangular geocentric z-coordinate
-        this._RA = Math.atan2(this._ye, this._xe);                  // Right Ascension
-        this._Dec = Math.atan2(this._ze, Math.sqrt(this._xe*this._xe+this._ye*this._ye)); // Declination
-        this._Ls = this._M + this._w                                // Mean longitude
+        this._RA = modulateCircle(Math.atan2(this._ye, this._xe));  // Right Ascension
+        this._Dec = modulateCircle(Math.atan2(this._ze, Math.sqrt(this._xe*this._xe+this._ye*this._ye))); // Declination
+        this._Ls = modulateCircle(this._M + this._w)                // Mean longitude
     }
     get pos() {
         return {x: this._x, y: this._y};
@@ -93,13 +94,13 @@ class planetElement extends astroElement {
         super(N, i, w, a, e, M, update);
         this._type = "planet"
     }
-    updateElement(d) {
-        super.updateElement(d);
+    elementAt(d) {
+        super.elementAt(d);
         if (Math.abs(this._e) > 0.05) {this.approxE();}                             // if orbit is elliptical, approximate bessel-function
         this._x = this._a * (Math.cos(this._E) - this._e);                          // x-coordinate on elliptic plane
         this._y = this._a * (Math.sqrt(1 - this._e*this._e) * Math.sin(this._E));   // y-coordinate on elliptic plane
         this._r = Math.sqrt(this._y*this._y+this._x*this._x);                       // distance from sun
-        this._v = Math.atan2(this._y, this._x);                                     // true anomaly (angle between position and perihelion)
+        this._v = modulateCircle(Math.atan2(this._y, this._x));                     // true anomaly (angle between position and perihelion)
         // 3D coordinates
         this._xh = this._r * (Math.cos(this._N) * Math.cos(this._v+this._w) - Math.sin(this._N) * Math.sin(this._v+this._w) * Math.cos(this._i));
         this._yh = this._r * (Math.sin(this._N) * Math.cos(this._v+this._w) + Math.cos(this._N) * Math.sin(this._v+this._w) * Math.cos(this._i));
@@ -114,7 +115,7 @@ class planetElement extends astroElement {
             delta = (E0 - this._e * Math.sin(E0) - this._M) / (1 - this._e * Math.cos(E0));
             E1 = E0 - delta;
         } while (getDegree(Math.abs(delta)) > 0.001);   // calculate to accuracy of 0.001 degrees
-        this._E = E1;                               // if orbit is too parabolical (e≈1), this will not converge!
+        this._E = modulateCircle(E1);                   // if orbit is too parabolical (e≈1), this will not converge!
     }
     get pos() {
         return {x: this._x, y: this._y};
@@ -221,15 +222,15 @@ const solarSystem = {
 /* functions for displaying the solar system */
 
 function updateSolarSystem(d) {
-    Object.keys(solarSystem).forEach(element => {
-        solarSystem[element].updateElement(d);
+    Object.keys(solarSystem).forEach(instance => {
+        solarSystem[instance].elementAt(d);
     });
 }
 
 function position(scale) {
-    Object.keys(solarSystem).forEach(element => {
-        document.getElementById(element).style.top = solarSystem[element].pos.y*scale+"px";
-        document.getElementById(element).style.left = solarSystem[element].pos.x*scale+"px";
+    Object.keys(solarSystem).forEach(instance => {
+        document.getElementById(instance).style.top = solarSystem[instance].pos.y*scale+"px";
+        document.getElementById(instance).style.left = solarSystem[instance].pos.x*scale+"px";
     });
 }
 
@@ -242,21 +243,39 @@ function frame() {
       d+=step;
       updateSolarSystem(d);
       position(scale);
-      document.getElementById("d").innerHTML = d;
+      document.getElementById("d").innerHTML = Math.round(solarSystem.mars._T);
     }
   }
 
 var anim = setInterval(frame, 10);
 
-function drawOrbit(element, dom) {
-    const canvas = dom;
+function drawOrbit(instance, dom, epoch=0, n=1000) {
+    const T = instance._P*y_earth;
+    var ellipse = [];
+    for (let i = 0; i < n; i++) {
+        instance.elementAt(epoch+i*T/n);
+        ellipse.push(instance.pos);
+    }
+    var ctx = dom;
+    var mars_orbit = new Chart(ctx, {
+        type: 'scatter',
+        data: ellipse,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    /*const canvas = dom;
     const ctx = canvas.getContext('2d');
     ctx.translate(canvas.width * 0.5, canvas.height * 0.5);
 
     // Draw the ellipse
     ctx.beginPath();
     ctx.ellipse(0, 0, 50, 75, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.stroke();*/
 }
 
 /* functions for html-interaction */
@@ -272,12 +291,35 @@ function plusScale() {
     scale += 10;
     updateScale();
 }
+function updateStep() {
+    document.getElementById("step-form").value = step;
+}
+function minusStep() {
+    step--;
+    updateStep();
+}
+function plusStep() {
+    step++;
+    updateStep();
+}
+function stopStep() {
+    step = 0;
+    updateStep();
+}
+
+function togglePlanet(name) {
+    if (document.getElementById(name).style.visibility == 'hidden'){
+        document.getElementById(name).style.visibility = 'visible';
+    } else {
+        document.getElementById(name).style.visibility = 'hidden';
+    }
+}
 
 /* settings */ 
 
 var d = -35000;
 
-var scale = 60;
+var scale = 20;
 position(scale);
 updateScale();
 document.getElementById("scale-minus").addEventListener("click", minusScale);
@@ -289,6 +331,16 @@ document.getElementById("scale-form").addEventListener("keyup", event => {
     } else {return;}
     event.preventDefault();
 });
-drawOrbit(mars, document.getElementById('mars-orbit'))
+document.getElementById("step-minus").addEventListener("click", minusStep);
+document.getElementById("step-plus").addEventListener("click", plusStep);
+document.getElementById("step-form").addEventListener("keyup", event => {
+    if(event.key == "Enter") {// Check for Enter-key
+    step = Number(document.getElementById("step-form").value);
+    updateStep();
+    } else {return;}
+    event.preventDefault();
+});
+drawOrbit(solarSystem.mars, document.getElementById('mars-orbit'), solarSystem.mars._T);
 
 var step = 1;
+updateStep();
